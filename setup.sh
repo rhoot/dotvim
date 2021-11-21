3046199a41adfb92f75c27971d5d3a84d9527384
Johan Sköld <git@johan.skold.cc>
2021-11-20 21:56:51 -0800

    Adding setup script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+add_symlink() {
+	local target="$1"
+	local link="$2"
+
+	if [ -h "$link" ]; then
+		rm "$link"
+	fi
+
+	if [ -e "$link" ]; then
+		printf "\033[31mERR: '%s' exists and would be overwritten\033[0m\n" "$link" > /dev/stderr
+	fi
+
+	ln -s "$target" "$link"
+}
+
+add_symlink "$script_dir/nvim" ~/.config/nvim
+add_symlink "$script_dir/vimrc" ~/.vimrc
+add_symlink "$script_dir/gvimrc" ~/.gvimrc
+
+rsync -r "$script_dir/fonts/" ~/.local/share/fonts
+
