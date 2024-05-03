7a82fb0dc02aa91c055e7d86fa1b97e49e248a4a
Johan Sköld <git@johan.skold.cc>
2024-05-02 18:53:25 -0700

    Set title

--- nvim/ginit.vim
+++ nvim/ginit.vim
@@ -8,11 +8,3 @@ end
 if exists(":GuiTabline")
 	:GuiTabline 0
 end
-
-" neovim-qt cwd title
-set title
-let &titlestring=fnamemodify(getcwd(), ":t") . " — nvim"
-augroup dirchange
-	autocmd!
-	autocmd DirChanged * let &titlestring=fnamemodify(v:event["cwd"], ":t") . " — nvim"
-augroup END
--- nvim/init/options.lua
+++ nvim/init/options.lua
@@ -1,3 +1,7 @@
+local function set_cwd_title(cwd)
+	vim.o.titlestring = vim.fn.fnamemodify(cwd, ":t").." — nvim"
+end
+
 if vim.fn.has("mac") ~= 0 then
 	vim.o.guifont = "JetBrains Mono:h12"
 else
@@ -5,3 +9,12 @@ else
 end
 
 vim.o.mouse = "nvi"
+vim.o.title = true
+
+set_cwd_title(vim.fn.getcwd())
+
+vim.api.nvim_create_augroup("dirchange", { clear = true })
+vim.api.nvim_create_autocmd("DirChanged", {
+	group = "dirchange",
+	callback = function() set_cwd_title(vim.v.event.cwd) end,
+})
