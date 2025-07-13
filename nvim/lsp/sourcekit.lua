98b427f28b714f71d73699fd9f7161f01731c620
Johan Sköld <git@johan.skold.cc>
2025-07-13 14:02:24 -0700

    Disable sourcekit inlay hints

--- nvim/lsp/sourcekit.lua
+++ nvim/lsp/sourcekit.lua
@@ -2,4 +2,8 @@ return {
 	cmd = { "sourcekit-lsp" },
 	filetypes = { "swift" },
 	root_markers = { "Package.swift", ".git" },
+	handlers = {
+		-- https://github.com/swiftlang/sourcekit-lsp/issues/2021
+		['textDocument/inlayHint'] = function() end,
+	},
 }
