77758605b5de3621b01b67eb9f5121259f662aef
Johan Sköld <git@johan.skold.cc>
2025-09-14 14:12:34 -0700

    Prioritized parent dir

--- nvim/lsp/omnisharp.lua
+++ nvim/lsp/omnisharp.lua
@@ -1,19 +1,25 @@
+local function find_parent_dir(path, pattern)
+	for dir in vim.fs.parents(path) do
+		for name, type in vim.fs.dir(dir) do
+			if name:match(pattern) then
+				return dir
+			end
+		end
+	end
+	return nil
+end
+
 return {
 	cmd = { "omnisharp", "-z", "--hostPID", tostring(vim.fn.getpid()), "DotNet:enablePackageRestore=false", "--encoding", "utf-8", "--languageserver" },
 	filetypes = { "cs" },
 	root_dir = function(bufnr, on_dir)
 		local fpath = vim.api.nvim_buf_get_name(bufnr)
-		for dir in vim.fs.parents(fpath) do
-			for name, type in vim.fs.dir(dir) do
-				if name:match("%.sln$")
-					or name:match("%.csproj$")
-					or name == "omnisharp.json"
-					or name == ".git"
-				then
-					on_dir(dir)
-					return
-				end
-			end
+		local parent_dir = find_parent_dir(fpath, "^omnisharp.json$")
+			or find_parent_dir(fpath, "%.sln$")
+			or find_parent_dir(fpath, "%.csproj$")
+			or find_parent_dir(fpath, "^.git$")
+		if parent_dir then
+			on_dir(parent_dir)
 		end
 	end,
 	capabilities = {
