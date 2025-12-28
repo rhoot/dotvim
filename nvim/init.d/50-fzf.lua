cb7c9bbc8f77c9aeeb1e6f5b6408b9df638880b0
Johan Sköld <git@johan.skold.cc>
2025-12-27 16:50:58 -0800

    Update packages

--- nvim/init.d/50-fzf.lua
+++ nvim/init.d/50-fzf.lua
@@ -18,6 +18,6 @@ fzf_lua.setup {
 }
 
 local cmdopts = { nargs='?', force=true }
-vim.api.nvim_create_user_command("Files", function() fzf_lua.files() end, cmdopts)
+vim.api.nvim_create_user_command("Files", function() fzf_lua.global() end, cmdopts)
 vim.api.nvim_create_user_command("Buffers", function() fzf_lua.buffers() end, cmdopts)
 vim.api.nvim_create_user_command("Ag", function(o) fzf_lua.grep({ search = o.args }) end, cmdopts)
